9d3bc5b073b946d7366abb2286f6c70fc07cbc19
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-20 17:10:59 +0200

    task 10

---
+++ 10.php
@@ -0,0 +1,19 @@
+<?php
+$day = 6;
+switch ($day) {
+    case 1 :
+        echo 'Это рабочий день';
+        break;
+    case 2 :
+        echo 'Это рабочий день';
+        break;
+    case 3 :
+        echo 'Это рабочий день';
+        break;
+    case 4 :
+        echo 'Это рабочий день';
+        break;
+    case 5 :
+        echo 'Это рабочий день';
+        break;
+}
