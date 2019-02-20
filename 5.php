00c04022a263bf9ec2dfb9bc4ecd93012f53ca74
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-20 16:13:19 +0200

    task 5

---
+++ 5.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>5</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+    $age = 6;
+    if ($age > 18 & $age<59) {
+        echo 'Вам еще работать и работать';
+    }
