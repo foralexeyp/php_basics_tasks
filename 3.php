dc5dc26466dd68b1176081d959e1effb9db4048e
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-20 14:29:00 +0200

    task 3

---
+++ 3.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>3</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+    $name = 'Алексей';
+    echo "Меня зовут: $name" ;
