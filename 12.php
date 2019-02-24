6ee61c1c8c5ec423345a5a08b4e8c6c6e7b863aa
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:10:42 +0200

    added task

--- 12.php
+++ 12.php
@@ -1,5 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>12</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>12. С помощью конструкции switch выведите фразу: "Неизвестный день", если значение переменной day <br>
+    не попадает в диапазон чисел от 1 до 7 (включительно).</p>
+
 <?php
-$day = 3;
+$day = 13;
 switch ($day) {
     case 1 :
         echo 'Это рабочий день';
