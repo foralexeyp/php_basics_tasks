c4da01bea1c574384d7b695ca32de4d02ed46034
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:04:37 +0200

    added task

--- 11.php
+++ 11.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>11</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>11. С помощью конструкции switch выведите фразу: "Это выходной день", если значение переменной day <br>
+    попадает в диапазон чисел от 6 до 7 (включительно).</p>
+
 <?php
 $day = 6;
 switch ($day) {
