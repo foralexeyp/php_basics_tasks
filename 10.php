501fca48ffe88b2fa04a8c40132a57ce1b3a873c
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:02:43 +0200

    added task

--- 10.php
+++ 10.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>10</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>10. С помощью конструкции switch выведите фразу: "Это рабочий день", если значение переменной day <br>
+    попадает в диапазон чисел от 1 до 5 (включительно).</p>
+
 <?php
 $day = 3;
 switch ($day) {
