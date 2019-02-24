4d03c5a8a6b95a443439612071ba903b42a59179
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:29:28 +0200

    added task

--- 21.php
+++ 21.php
@@ -1,3 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>21</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>21. Приведите число 0 к типу boolean. Объясните результат.</p>
+
 <?php
 $a = 0;
 echo (bool)$a;
