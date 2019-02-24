5187c2d40e89841c1fb1c0cd14aeab04c9351416
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:14:47 +0200

    added task

--- 14.php
+++ 14.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>13</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>14. Задана переменная $foo = 'bar'; Создать переменную bar, в которой будет храниться число 10.<br>
+    Вывести число 10, используя только переменную $foo (не опечатка).</p>
+
 <?php
 $foo = 'bar';
 $bar = 10;
