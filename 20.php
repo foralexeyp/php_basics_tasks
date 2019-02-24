24ff8a91719fc513941d3413967bf2db08fb6309
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:27:52 +0200

    added task

--- 20.php
+++ 20.php
@@ -1,3 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>20</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>20. Приведите число 20 к типу boolean. Объясните результат.</p>
+
 <?php
 $a = 20;
 echo (bool)$a;
