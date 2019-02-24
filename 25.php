4f20cfdd7476c1ffa4a9b0d8a50fef160e0fb5fd
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:34:41 +0200

    added task

--- 25.php
+++ 25.php
@@ -1,3 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>25</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>25. Приведите пример, чем отличается &lt;?php от &lt;?=.</p>
 
 <?php
 $a = 'TEXT_1';
