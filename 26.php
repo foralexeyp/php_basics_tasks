2d7e4383038ac1bbcfd3a6a0bd13c71150a7f6d7
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:37:24 +0200

    added task

--- 26.php
+++ 26.php
@@ -1,7 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>26</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>26. Объявите константу DAYS_COUNT равную 7 <br>
+    и константу MONTH_COUNT равную 12 двумя разными способами объявления констант.</p>
+
 <?php
 define("DAYS_CONST", 7);
 echo DAYS_CONST;
 echo '<br>';
 
 const MONTH_COUNT = 12;
-echo constant('MONTH_COUNT');
+echo MONTH_COUNT;
+//echo constant('MONTH_COUNT');
