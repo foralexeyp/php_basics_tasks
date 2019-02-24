7c73c19d8b481cc374ef4d6417468d4424b951c0
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 13:03:16 +0200

    added task

--- 9.php
+++ 9.php
@@ -1,2 +1,16 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>9</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>9. Создайте переменную day и присвойте ей произвольное числовое значение.</p>
+
 <?php
 $day = 3;
