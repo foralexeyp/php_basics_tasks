7a0aaac3bb975abba9bb99a6903f4d475e1239d9
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 14:32:02 +0200

    added task

--- 23.php
+++ 23.php
@@ -1,3 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>23</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<p>23. Напишите 3 строки, используя три разные функции для вывода текста.</p>
+
 <?php
 $s = 'Какой-то текст';
 echo $s;
