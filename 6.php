4fd375f78c0037b6eeeef25e196c403ee7671ca5
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-24 12:17:37 +0200

    added task

--- 6.php
+++ 6.php
@@ -1,6 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>6</title>
+</head>
+<body>
+
+</body>
+</html>
+<p>6. Расширьте конструкцию if из п.5, выводя фразу: "Вам пора на пенсию" при условии, <br>
+    что значение переменной age больше 59.</p>
+
 <?php
-    $age = 46;
-    if ($age >= 18) {
+    $age = 20;
+    if ($age >= 18 && $age <= 59) {
         echo 'Вам еще работать и работать';
     }
     elseif ($age > 59) {
